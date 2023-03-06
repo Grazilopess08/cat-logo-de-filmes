883b303c55ef7f6c21d30699becb00f3cf4458c7
Grazielly Lopes <grazilly-ltdsantos@educar,rs,gov,br>
2023-03-06 15:21:16 -0300

    commit 2

---
+++ js/classes.js
@@ -0,0 +1,31 @@
+class Ator
+{
+    constructor(id, nome){
+        this.nome=nome;
+        this.id-id;
+    }
+}
+
+class Diretor
+{
+    constructor(id, nome){
+        this.nome=nome;
+        this.id-id;
+    }
+}
+
+class Filmes
+{
+    constructor(id, titulo, ano, categoria, cartaz, direcao, atores, classificacao, avaliacao){
+        this.id-id;
+        this.titulo=titulo;
+        this.ano=ano;
+        this.categoria=categoria;
+        this.cartaz=cartaz;
+        this.direcao=direcao;
+        this.atores=atores;
+        this.classificacao=classificacao;
+        this.avaliacao=avaliacao;
+
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,7 @@
+let ator = new Ator(1, "JOHN WAYNE")
+
+console.log(ator);
+
+let diretor = new Diretor(1, "Alfred Hitchcock")
+
+console.log(diretor);
